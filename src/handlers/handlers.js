a7681fdc3d95e3429a31a91462fbc163b226ddd7
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2022-04-28 12:09:50 +0300

    specifying the node engine

--- src/handlers/handlers.js
+++ src/handlers/handlers.js
@@ -31,6 +31,7 @@ const getOtpHandler = async (req, res) => {
         const { error } = await supabase.from('otps').select('phone_number').eq('phone_number',phone_number)
         if( error ) {
             console.log('reached')
+            console.log(res)
             res.json(error)
         } else {
             // Updating the verification code against the number.
@@ -44,6 +45,7 @@ const getOtpHandler = async (req, res) => {
                 console.log('else started')
                 sendCodeToPhone(phone_number, otp)
                 .then((data) => {
+                    
                     res.status(200).json(data)
                 })
                 .catch(error => {
